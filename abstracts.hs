f0108c02bc7b4154f83a0c6e0620c09858d5071e
James Yoo <jyoo980@gmail.com>
2018-01-28 22:40:46 -0800

    Added more abstracts

--- abstracts.hs
+++ abstracts.hs
@@ -3,3 +3,25 @@
 myFoldr :: (x -> y -> y) -> y -> [x] -> y
 myFoldr _ b [] = b
 myFoldr f b (x:xs) = f x (myFoldr f b xs)
+
+myMap :: (x -> y) -> [x] -> [y]
+myMap _ [] = []
+myMap f (x:xs) = f x : myMap f xs
+
+myFilter :: (x -> Bool) -> [x] -> [x]
+myFilter _ [] = []
+myFilter pred (x:xs) = 
+    if pred x
+        then x : myFilter pred xs
+        else
+            myFilter pred xs
+
+andMap :: (x -> Bool) -> [x] -> Bool
+myFilter _ [] = True
+myFilter pred (x:xs) =
+    pred x && myFilter pred xs
+
+orMap :: (x -> Bool) -> [x] -> Bool
+orMap _ [] = False
+orMap pred (x:xs) =
+    pred x || orMap pred xs
