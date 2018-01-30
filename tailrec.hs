e9285986c612f5c1c7724c1517eb3daec2d9b985
James Yoo <jyoo980@gmail.com>
2018-01-30 00:21:09 -0800

    Rewriting abstract functions

--- tailrec.hs
+++
@@ -1,3 +0,0 @@
-{- A lot of the functions I've written so far are head-recursive, not ideal when you're concerned 
-    about stack memory. In this file, I try to write tail-recursive implementations of simple functions-}
-
