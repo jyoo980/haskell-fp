b29223520dcce9d9c588aef7c2c06c591cc84bf7
James Yoo <jyoo980@gmail.com>
2018-01-30 20:47:50 -0800

    Added head'

--- fold-stdlib.hs
+++ fold-stdlib.hs
@@ -2,3 +2,9 @@
 
 maximum' :: (Ord a) => [a] -> a
 maximum' = foldl1 (\x acc -> if x > acc then x else acc)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) [] 
+
+head' :: [a] -> a
+head' = foldr1 (\x _ -> x)
