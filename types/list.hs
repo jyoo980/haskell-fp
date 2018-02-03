5445c94323410d0e0d8296f468e65c8b9003394e
jyoo980 <jyoo980@gmail.com>
2018-02-02 22:25:41 -0800

    Added (.++), custom append

--- types/list.hs
+++ types/list.hs
@@ -7,3 +7,9 @@ infixr 5 :-:
 data List a = Empty | a :-: (List a) deriving (Show, Eq, Ord, Read)
 
 test = 1 :-: 2 :-: 3 :-: Empty
+
+-- Let's create our own append function (.++)
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys               
+(x :-: xs) .++ ys = x :-: (xs .++ ys) 
