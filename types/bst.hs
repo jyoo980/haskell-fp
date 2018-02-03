507754ad4c90ee18cb6aa8c073ce3efe85a65f7f
jyoo980 <jyoo980@gmail.com>
2018-02-02 23:23:46 -0800

    Fixed error with tree func

--- types/bst.hs
+++ types/bst.hs
@@ -11,11 +11,11 @@ insertInto :: (Ord a) => a -> Tree a -> Tree a
 insertInto x EmptyTree = singleton x
 insertInto x (Node a left right)
     | x == a = Node x left right
-    | x < a = Node x (insertInto x left) right
-    | x > a = Node x left (insertInto x right)
+    | x < a = Node a (insertInto x left) right
+    | x > a = Node a left (insertInto x right)
 
 -- Searching for a value in a BST
-isPresent :: (Ord a) => a -> Bool
+isPresent :: (Ord a) => a -> Tree a -> Bool
 isPresent x EmptyTree = False
 isPresent x (Node a left right)
     | x == a = True
