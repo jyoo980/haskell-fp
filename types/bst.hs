28fd283d2cb80a009635b8c375982da983466109
jyoo980 <jyoo980@gmail.com>
2018-02-02 22:38:46 -0800

    Added singleton method

--- types/bst.hs
+++ types/bst.hs
@@ -1,3 +1,7 @@
 -- This file contains an example implementation of a Binary Search Tree
 
-data Tree a = emptyTree | Node a (Tree a) (Tree a) deriving (Show, Eq, Ord)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Eq, Ord)
+
+-- We need some function that we can use to initially create a simple tree
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
