ec014ac18f7ae248099f41bff8c005416c60e54c
jyoo980 <jyoo980@gmail.com>
2018-02-02 22:58:12 -0800

    Added isPresent.hs

--- types/bst.hs
+++ types/bst.hs
@@ -5,3 +5,19 @@ data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Eq, Ord)
 -- We need some function that we can use to initially create a simple tree
 singleton :: a -> Tree a
 singleton x = Node x EmptyTree EmptyTree
+
+-- Inserting into correct place (BST)
+insertInto :: (Ord a) => a -> Tree a -> Tree a
+insertInto x EmptyTree = singleton x
+insertInto x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node x (insertInto x left) right
+    | x > a = Node x left (insertInto x right)
+
+-- Searching for a value in a BST
+isPresent :: (Ord a) => a -> Bool
+isPresent x EmptyTree = False
+isPresent x (Node a left right)
+    | x == a = True
+    | x < a = isPresent x left
+    | x > a = isPresent x right
