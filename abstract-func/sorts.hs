fabf9d9c40f2bac3f4e82f7213587b91d246e2e2
jyoo980 <jyoo980@gmail.com>
2018-02-04 18:32:42 -0800

    Began implementation of mergesort

--- abstract-func/sorts.hs
+++ abstract-func/sorts.hs
@@ -1,7 +1,16 @@
-{- This file will contain implementations for sorting algorithms -}
-
+-- Quicksort
+-- Some may say that this is not a "true" implementation, since do not sort in-place
 quickSort :: (Ord x) => [x] -> [x]
 quickSort [] = []
 quickSort (x:xs) = quickSort (filter (<x) xs)
                    ++ [x] ++
                    quickSort (filter (>x) xs)
+
+-- Mergesort
+fstHalf :: (Ord a) => [a] -> [a]
+fstHalf [] = []
+fstHalf xs = take (length xs `div` 2) xs
+
+sndHalf :: (Ord a) => [a] -> [a]
+sndHalf [] = []
+sndHalf xs = drop (length xs `div` 2) xs
