20a752aa06b96f6a0bcb5d1fd55aa6157e32b0af
James Yoo <jyoo980@gmail.com>
2018-02-04 23:09:04 -0800

    Fixed Hlint errors

--- abstract-func/sorts.hs
+++ abstract-func/sorts.hs
@@ -20,8 +20,8 @@ merge :: (Ord a) => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
 merge (x:xs) (y:ys)
-    | x <= y = x : (merge xs (y:ys))
-    | otherwise = y : (merge (x:xs) ys)
+    | x <= y = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
 
 mergeSort :: (Ord a) => [a] -> [a]
 mergeSort [] = []
